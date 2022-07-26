a9786e1a969e341294bb9a8153e19fddf87a2ab6
preston <kpreston313@gmail.com>
2022-07-25 19:14:28 -0700

    update header

--- wp-content/themes/beauty-salon-spa/header.php
+++ wp-content/themes/beauty-salon-spa/header.php
@@ -41,28 +41,31 @@
 								<div class="logo py-3 py-md-0">
 							        <?php if ( has_custom_logo() ) : ?>
 					            		<?php the_custom_logo(); ?>
-						            <?php endif; ?>
-					              	<?php $blog_info = get_bloginfo( 'name' ); ?>
-					              		<?php if( get_theme_mod('beauty_salon_spa_logo_title',true) != '' ){ ?>
-						                <?php if ( ! empty( $blog_info ) ) : ?>
-						                  	<?php if ( is_front_page() && is_home() ) : ?>
-						                    	<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-						                  	<?php else : ?>
-					                      		<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-					                  		<?php endif; ?>
-						                <?php endif; ?>
-						            <?php }?>
-						                <?php
-					                  		$description = get_bloginfo( 'description', 'display' );
-						                  	if ( $description || is_customize_preview() ) :
-						                ?>
-						                <?php if( get_theme_mod('beauty_salon_spa_logo_text',true) != '' ){ ?>
-					                  	<p class="site-description">
-					                    	<?php echo esc_html($description); ?>
-					                  	</p>
-					                  <?php }?>
-					              	<?php endif; ?>
-							    </div>
+					            <?php endif; ?>
+
+			              	<?php $blog_info = get_bloginfo( 'name' ); ?>
+
+		              		<?php if( get_theme_mod('beauty_salon_spa_logo_title',true) != '' ){ ?>
+				                <?php if ( ! empty( $blog_info ) ) : ?>
+				                  	<?php if ( is_front_page() && is_home() ) : ?>
+				                    	<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				                  	<?php else : ?>
+			                      		<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+			                  		<?php endif; ?>
+				                <?php endif; ?>
+					            <?php }?>
+					            
+				                <?php
+			                  		$description = get_bloginfo( 'description', 'display' );
+				                  	if ( $description || is_customize_preview() ) :
+				                ?>
+				                <?php if( get_theme_mod('beauty_salon_spa_logo_text',true) != '' ){ ?>
+			                  	<p class="site-description">
+			                    	<?php echo esc_html($description); ?>
+			                  	</p>
+			                  <?php }?>
+			              	<?php endif; ?>
+						    </div>
 							</div>
 							<div class="col-lg-7 col-md-4 col-sm-6 col-4 align-self-center">
 								<?php if(has_nav_menu('primary')){?>
